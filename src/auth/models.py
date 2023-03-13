69b56888fcf6ad5742073628bc1cd9ccd2b6ec40
baydinvladislav <baydinvladislav@yandex.ru>
2023-03-13 21:29:28 +0300

    auth service: nullable fields

--- src/auth/models.py
+++ src/auth/models.py
@@ -14,9 +14,9 @@ class User(Base, BaseModel):
     """
     username = Column("username", String(100), nullable=False)
     password = Column("password", String, nullable=False)
-    first_name = Column("first_name", String(50), nullable=False)
-    last_name = Column("last_name", String(50), nullable=False)
-    gender: Column = Column("gender", Enum(Gender), nullable=False)
+    first_name = Column("first_name", String(50), nullable=True)
+    last_name = Column("last_name", String(50), nullable=True)
+    gender: Column = Column("gender", Enum(Gender), nullable=True)
 
     # customers = relationship("Customer", cascade="all,delete-orphan", back_populates="user")
     # exercises = relationship("Exercise", cascade="all,delete-orphan", back_populates="user")
