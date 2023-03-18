7eca3c463c9e0ba407fd5b4fa58308a1ba96857b
baydinvladislav <baydinvladislav@yandex.ru>
2023-03-18 13:54:32 +0300

    auth service: pylint

---
+++ src/auth/dependencies.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+from typing import Any, Union
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt
+from pydantic import ValidationError
+
+from src.auth.schemas import SystemUser, TokenPayload
+from src.auth.utils import ALGORITHM, JWT_SECRET_KEY
+
+reuseable_oauth = OAuth2PasswordBearer(
+    tokenUrl="/login",
+    scheme_name="JWT"
+)
+
+
+async def get_current_user(token: str = Depends(reuseable_oauth)) -> SystemUser:
+    try:
+        payload = jwt.decode(
+            token, JWT_SECRET_KEY, algorithms=[ALGORITHM]
+        )
+        token_data = TokenPayload(**payload)
+
+        if datetime.fromtimestamp(token_data.exp) < datetime.now():
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Token expired",
+                headers={"WWW-Authenticate": "Bearer"}
+            )
+
+    except (jwt.JWTError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+
+    user = token_data.sub
+
+    # user: Union[dict[str, Any], None] = db.get(token_data.sub, None)
+
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Could not find user"
+        )
+
+    return SystemUser(**user)
--- src/auth/router.py
+++ src/auth/router.py
@@ -6,12 +6,12 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
-from src.auth.dependencies import get_current_user
-from src.auth.models import User
-from src.auth.schemas import TokenSchema, UserRegisterIn, UserRegisterOut
-from src.auth.utils import (create_access_token, create_refresh_token,
-                            get_hashed_password, verify_password)
 from src.dependencies import get_db
+from .dependencies import get_current_user
+from .models import User
+from .schemas import TokenSchema, UserRegisterIn, UserRegisterOut
+from .utils import (create_access_token, create_refresh_token,
+                            get_hashed_password, verify_password)
 
 auth_router = APIRouter()
 
