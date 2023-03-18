b3eef197f53ddf701938198473c9cc107c0b2e62
baydinvladislav <baydinvladislav@yandex.ru>
2023-03-18 11:45:49 +0300

    auth service: test is passed

--- tests/auth/test_signup.py
+++ tests/auth/test_signup.py
@@ -1,14 +1,18 @@
 from fastapi.testclient import TestClient
 
+from src.auth.utils import get_hashed_password
 from src.main import app
 
 
 def test_signup_new_user():
     with TestClient(app) as client:
-        json = {
+        body = {
             "username": "79857994488",
-            "password": "my_pswd"
+            "password": get_hashed_password("my_pswd")
         }
 
-        response = client.post("/signup", json=json)
+        response = client.post("/signup", json=body)
         assert response.status_code == 201
+
+        assert "id" in response.json()
+        assert response.json()["username"] == body["username"]
