d7d9f26607497df9b39aa2fab7b62402a50eaca5
baydinvladislav <baydinvladislav@yandex.ru>
2023-03-25 13:02:22 +0300

    auth_service: async tests are passed

--- tests/auth/test_signup.py
+++ tests/auth/test_signup.py
@@ -7,7 +7,10 @@ from tests.conftest import TEST_USER_PASSWORD, TEST_USER_USERNAME
 
 
 @pytest.mark.anyio
-async def test_successfully_signup(override_get_db):
+async def test_signup_successfully(override_get_db):
+    """
+    Success registration
+    """
     user = override_get_db.query(User).filter(
         User.username == TEST_USER_USERNAME
     ).first()
@@ -25,3 +28,53 @@ async def test_successfully_signup(override_get_db):
         response = await ac.post("/signup", json=body)
 
     assert response.status_code == 201
+
+
+@pytest.mark.anyio
+async def test_signup_validation_error(override_get_db):
+    """
+    Failed registration because of validation error
+    """
+    user = override_get_db.query(User).filter(
+        User.username == TEST_USER_USERNAME
+    ).first()
+
+    if user:
+        override_get_db.delete(user)
+        override_get_db.commit()
+
+    body = {
+        # without "+"
+        "username": "79850002233",
+        "password": TEST_USER_PASSWORD
+    }
+
+    async with AsyncClient(app=app, base_url="http://as-coach") as ac:
+        response = await ac.post("/signup", json=body)
+
+    assert response.status_code == 422
+
+
+@pytest.mark.anyio
+async def test_signup_too_short_password(override_get_db):
+    """
+    Failed registration because of validation error
+    """
+    user = override_get_db.query(User).filter(
+        User.username == TEST_USER_USERNAME
+    ).first()
+
+    if user:
+        override_get_db.delete(user)
+        override_get_db.commit()
+
+    body = {
+        "username": TEST_USER_USERNAME,
+        # password is less 8 symbols
+        "password": "1234567"
+    }
+
+    async with AsyncClient(app=app, base_url="http://as-coach") as ac:
+        response = await ac.post("/signup", json=body)
+
+    assert response.status_code == 422
