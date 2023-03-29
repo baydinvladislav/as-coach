5036c125c39024483b2bcd32520a16616643a361
baydinvladislav <baydinvladislav@yandex.ru>
2023-03-29 14:25:29 +0300

    mobile_init: before change project structure

--- src/main.py
+++ src/main.py
@@ -3,11 +3,28 @@ Application entrypoint.
 """
 
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from src.auth.router import auth_router
 
-app = FastAPI(title="As Coach")
-app.include_router(auth_router)
+
+def get_application():
+    as_coach = FastAPI(title="As Coach")
+
+    as_coach.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+    )
+
+    as_coach.include_router(auth_router, prefix="/api")
+
+    return as_coach
+
+
+app = get_application()
 
 
 @app.get("/")
