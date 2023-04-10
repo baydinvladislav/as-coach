c67d5877d65d85c4a899ba4b5087023e128d5552
baydinvladislav <baydinvladislav@yandex.ru>
2023-04-10 18:32:14 +0300

    gym: gym schemas

--- backend/src/customer/schemas.py
+++ backend/src/customer/schemas.py
@@ -2,11 +2,12 @@
 Schemas for customer service
 """
 
-from typing import Optional
+from typing import Optional, List, Union
 
 from pydantic import BaseModel, validator
 
 from src.utils import validate_phone_number
+from src.gym.schemas import Diet, Training
 
 
 class CustomerCreateIn(BaseModel):
@@ -32,13 +33,22 @@ class CustomerOut(CustomerCreateIn, BaseModel):
 
 class TrainingPlanIn(BaseModel):
     """
-
+    Income JSON for creating training plan for user
     """
-    pass
+    start_date: str
+    end_date: str
+    diets: List[Diet]
+    trainings: Union[List[Training], List[None]]
 
 
 class TrainingPlanOut(BaseModel):
     """
-
+    Output JSON after successfully training plan creation
     """
-    pass
+    id: str
+    start_date: str
+    end_date: str
+    number_of_trainings: int
+    proteins: int
+    fats: int
+    carbs: int
--- backend/src/gym/schemas.py
+++ backend/src/gym/schemas.py
@@ -1,3 +1,29 @@
 """
 
 """
+
+from typing import List
+
+from pydantic import BaseModel
+
+
+class Diet(BaseModel):
+    """
+    """
+    proteins: int
+    fats: int
+    carbs: int
+
+
+class Exercise(BaseModel):
+    """
+    """
+    id: str
+    sets: List[int]
+
+
+class Training(BaseModel):
+    """
+    """
+    name: str
+    exercises: List[Exercise]
