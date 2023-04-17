0d731182350dd45e4bb3dea353b3eaa0baff54e7
baydinvladislav <baydinvladislav@yandex.ru>
2023-04-17 12:24:59 +0300

    gym_service: muscle groups controller

--- backend/src/gym/router.py
+++ backend/src/gym/router.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import Session
 
 from src.dependencies import get_db
 from src.auth.dependencies import get_current_user
-from src.gym.models import Exercise
+from src.gym.models import Exercise, MuscleGroup
 from src.gym.schemas import ExerciseCreateIn, ExerciseCreateOut
 
 gym_router = APIRouter()
@@ -77,6 +77,35 @@ async def get_exercises(
             "id": str(exercise.id),
             "muscle_group": exercise.muscle_group.name,
             "name": exercise.name
+        })
+
+    return response
+
+
+@gym_router.get(
+    "/muscle_groups",
+    summary="Returns all muscle groups",
+    status_code=status.HTTP_200_OK)
+async def get_muscle_groups(
+        database: Session = Depends(get_db),
+        current_user: Session = Depends(get_current_user)) -> list:
+    """
+    Returns all muscle groups for user
+
+    Args:
+        database: dependency injection for access to database
+        current_user: returns current application user
+
+    Returns:
+        list of muscle groups
+    """
+    muscle_groups = database.query(MuscleGroup).all()
+
+    response = []
+    for muscle_group in muscle_groups:
+        response.append({
+            "id": str(muscle_group.id),
+            "name": muscle_group.name
         })
 
     return response
