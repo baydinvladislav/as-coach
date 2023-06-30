5c5a01ff275bb4a32be9919348d2c532ee9f8f43
baydinvladislav <baydinvladislav@yandex.ru>
2023-06-30 21:47:29 +0300

    first endpoint with async db session

--- backend/src/database.py
+++ backend/src/database.py
@@ -6,6 +6,7 @@ import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
 from dotenv import load_dotenv
@@ -18,7 +19,7 @@ if bool(TEST_ENV):
     TEST_DATABASE_URL = os.environ.get("TEST_DATABASE_URL")
     engine = create_engine(str(TEST_DATABASE_URL))
 else:
-    engine = create_engine(str(DATABASE_URL))
+    engine = create_async_engine(DATABASE_URL, echo=True)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 Base = declarative_base()
--- backend/src/dependencies.py
+++ backend/src/dependencies.py
@@ -5,12 +5,12 @@ Common dependencies for application
 from src.database import SessionLocal
 
 
-def get_db():
+async def get_db():
     """
     Creates new database session.
     """
-    database = SessionLocal()
-    try:
-        yield database
-    finally:
-        database.close()
+    async with SessionLocal() as database:
+        try:
+            yield database
+        finally:
+            await database.close()
