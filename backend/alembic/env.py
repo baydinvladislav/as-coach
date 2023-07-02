93cd4c06e025434c586add2c7970944571e4a9ca
baydinvladislav <baydinvladislav@yandex.ru>
2023-07-02 22:46:34 +0300

    trying to apply async migrations

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -62,7 +62,7 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
-def run_migrations_online() -> None:
+async def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
