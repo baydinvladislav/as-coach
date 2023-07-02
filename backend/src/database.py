f14f9b6defaedb885dac98f49d9ab6bffc0d2d61
baydinvladislav <baydinvladislav@yandex.ru>
2023-07-02 21:58:40 +0300

    moved to async test db too

--- backend/src/database.py
+++ backend/src/database.py
@@ -4,7 +4,6 @@ Database settings module
 
 import os
 
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession  # type: ignore
 from sqlalchemy.orm import sessionmaker
@@ -17,7 +16,7 @@ from .config import DATABASE_URL, TEST_ENV
 if bool(TEST_ENV):
     load_dotenv()
     TEST_DATABASE_URL = os.environ.get("TEST_DATABASE_URL")
-    engine = create_engine(str(TEST_DATABASE_URL))
+    engine = create_async_engine(str(TEST_DATABASE_URL))
 else:
     engine = create_async_engine(DATABASE_URL, echo=True)
 
