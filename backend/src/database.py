3af79a1511d92bb81a4ca82b51b1cde818b5e15a
baydinvladislav <baydinvladislav@yandex.ru>
2023-07-10 20:24:38 +0300

    tests in test_signup.py are passed

--- backend/src/database.py
+++ backend/src/database.py
@@ -7,6 +7,7 @@ import os
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession  # type: ignore
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
 
 from dotenv import load_dotenv
 
@@ -16,9 +17,16 @@ from .config import DATABASE_URL, TEST_ENV
 if bool(TEST_ENV):
     load_dotenv()
     TEST_DATABASE_URL = os.environ.get("TEST_DATABASE_URL")
-    engine = create_async_engine(str(TEST_DATABASE_URL))
+    engine = create_async_engine(
+        str(TEST_DATABASE_URL), future=True, echo=False, poolclass=NullPool
+    )
 else:
-    engine = create_async_engine(DATABASE_URL, echo=True)
+    engine = create_async_engine(
+        DATABASE_URL, future=True, echo=False, poolclass=NullPool
+    )
 
-SessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+SessionLocal = sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession
+)
+
 Base = declarative_base()
