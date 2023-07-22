f97a09d85a8af34698b5603b69afffe423ddfc21
baydinvladislav <baydinvladislav@yandex.ru>
2023-07-22 08:21:44 +0300

    fix root test

--- backend/tests/tests.py
+++ backend/tests/tests.py
@@ -4,7 +4,7 @@ from httpx import AsyncClient
 from src.main import app
 
 
-@pytest.mark.anyio
+@pytest.mark.asyncio
 async def test_root(override_get_db):
     async with AsyncClient(app=app, base_url="http://as-coach") as ac:
         response = await ac.get("/")
