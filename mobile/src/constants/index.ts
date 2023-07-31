9f58b8df1cfe179bf00850aada337928146ed079
Impeqq <sokovnin.m@list.ru>
2023-07-31 17:46:12 +0400

    feat: added features for deploy

--- mobile/src/constants/index.ts
+++ mobile/src/constants/index.ts
@@ -1,14 +1,16 @@
+import Config from 'react-native-config';
+
 import { normVert } from '@theme';
 
 export * from './mask';
 export * from './regexp';
 export * from './date';
 
-export const API_URL_IOS = 'http://localhost:8000/api';
-export const API_URL_ANDROID = 'http://10.0.2.2:8000/api';
+export const API_URL_IOS = Config.API_URL;
+export const API_URL_ANDROID = Config.API_URL;
 
-export const URL_IOS = 'http://localhost:8000';
-export const URL_ANDROID = 'http://10.0.2.2:8000';
+export const URL_IOS = Config.API_URL;
+export const URL_ANDROID = Config.API_URL;
 
 export const ENCRYPTION_KEY = 'development';
 export const TOKEN = 'token';
