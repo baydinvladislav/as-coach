60b4ec43f080f049d688eaf8e4c621b398844e30
baydinvladislav <baydinvladislav@yandex.ru>
2023-08-03 10:39:47 +0300

    async migrations

--- backend/src/database.py
+++ backend/src/database.py
@@ -5,7 +5,7 @@ Database settings module
 import os
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, AsyncEngine  # type: ignore
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession  # type: ignore
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import NullPool
 
@@ -17,13 +17,13 @@ from .config import DATABASE_URL, TEST_ENV
 if bool(TEST_ENV):
     load_dotenv()
     TEST_DATABASE_URL = os.environ.get("TEST_DATABASE_URL")
-    engine = AsyncEngine(create_async_engine(
+    engine = create_async_engine(
         str(TEST_DATABASE_URL), future=True, echo=False, poolclass=NullPool
-    ))
+    )
 else:
-    engine = AsyncEngine(create_async_engine(
+    engine = create_async_engine(
         DATABASE_URL, future=True, echo=False, poolclass=NullPool
-    ))
+    )
 
 SessionLocal = sessionmaker(
     engine, expire_on_commit=False, class_=AsyncSession
