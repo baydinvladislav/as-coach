508fb5d7fc85703953f60edc18c24d17ed89545a
baydinvladislav <baydinvladislav@yandex.ru>
2023-08-23 14:53:32 +0300

    ac-0009: profile service add get_me method

--- backend/src/core/services/profile.py
+++ backend/src/core/services/profile.py
@@ -31,19 +31,23 @@ class ProfileService:
         self.customer_service = customer_service
 
     async def authorize_user(self, form_data: OAuth2PasswordRequestForm):
-        coach = await self.coach_service.find_coach_by_username(form_data.username)
-        customer = await self.customer_service.find_customer_by_username(form_data.username)
-
-        if not coach and not customer:
-            raise UserDoesNotExist
+        found_roles_by_username = await self.get_me(form_data.username)
+        coach, customer = found_roles_by_username["coach"], found_roles_by_username["customer"]
 
         if coach:
             await self.coach_service.authorize_coach(coach, form_data.password)
             return coach, "coach"
-        else:
+        elif customer:
             await self.customer_service.authorize_customer(customer, form_data.password)
             return customer, "customer"
+        else:
+            raise UserDoesNotExist
 
     async def register_user(self, data: CoachRegisterIn):
         coach = await self.coach_service.register_coach(data)
         return coach
+
+    async def get_me(self, username):
+        coach = await self.coach_service.find_coach_by_username(username)
+        customer = await self.customer_service.find_customer_by_username(username)
+        return {"coach": coach, "customer": customer}
