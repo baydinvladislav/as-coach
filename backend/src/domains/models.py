a95219e08a5d5e7c8db80940b26d4e44c2a883c9
baydinvladislav <baydinvladislav@yandex.ru>
2023-12-16 12:06:55 +0300

    [push_notifications] asc-0072: add fcm token column to users

--- backend/src/domains/models.py
+++ backend/src/domains/models.py
@@ -56,6 +56,8 @@ class Coach(Base, BaseModel):
         cascade="all,delete-orphan",
         back_populates="coach"
     )
+    # TODO: nullable False
+    fcm_token = Column("fcm_token", String(255), nullable=True)
 
     def __repr__(self):
         return f"Coach: {self.username}"
@@ -83,6 +85,8 @@ class Customer(Base, BaseModel):
     birthday = Column("birthday", Date, nullable=True)
     photo_path = Column("photo_path", String(255), nullable=True)
     email = Column("email", String(100), nullable=True)
+    # TODO: nullable False
+    fcm_token = Column("fcm_token", String(255), nullable=True)
 
     def __repr__(self):
         return f"Customer: {self.last_name} {self.first_name}"
