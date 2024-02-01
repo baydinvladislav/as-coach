a725c841f5022e5d1ab06a9648f7018354781863
Vlad Baydin <baydinvladislav@yandex.ru>
2024-02-01 22:20:13 +0300

    badge itself is responsible for its state (#105)

--- mobile/src/ui/badge.tsx
+++ mobile/src/ui/badge.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import moment from 'moment';
 import styled from 'styled-components';
 
+import { t } from '@i18n';
 import { colors, normHor, normVert } from '@theme';
 
 import { FontSize, FontWeight } from '~types';
@@ -17,22 +19,62 @@ export enum BadgeStatuses {
 }
 
 type TProps = {
-  text: string;
-  status: BadgeStatuses;
+  dateEnd: string;
 };
 
-export const Badge = ({ text, status }: TProps) => (
-  <Background status={status}>
-    <Text
-      style={{ textTransform: 'uppercase' }}
-      fontSize={FontSize.S10}
-      color={switchFontColor(status)}
-      weight={FontWeight.Bold}
-    >
-      {text}
-    </Text>
-  </Background>
-);
+export const Badge: React.FC<TProps> = ({ dateEnd }) => {
+  const getStatusAndText = () => {
+    if (!dateEnd) {
+      return {
+        status: BadgeStatuses.PLAN_NOT_EXISTS,
+        text: t('lk.customerStatus.noPlan'),
+      };
+    } else {
+      const currentDate = moment();
+      const dateCompletion = moment(dateEnd);
+      const duration = moment.duration(dateCompletion.diff(currentDate));
+      const differenceInDays = Math.round(duration.asDays());
+
+      if (differenceInDays > 3) {
+        return {
+          status: BadgeStatuses.GOOD,
+          text: t('lk.customerStatus.expiring', {
+            days: differenceInDays,
+          }),
+        };
+      } else if (differenceInDays > 0) {
+        return {
+          status: BadgeStatuses.WARNING,
+          text: t('lk.customerStatus.expiring', {
+            days: differenceInDays,
+          }),
+        };
+      } else {
+        return {
+          status: BadgeStatuses.EXPIRED,
+          text: t('lk.customerStatus.expired', {
+            days: Math.abs(differenceInDays),
+          }),
+        };
+      }
+    }
+  };
+
+  const { status, text } = getStatusAndText();
+
+  return (
+    <Background status={status}>
+      <Text
+        style={{ textTransform: 'uppercase' }}
+        fontSize={FontSize.S10}
+        color={switchFontColor(status)}
+        weight={FontWeight.Bold}
+      >
+        {text}
+      </Text>
+    </Background>
+  );
+};
 
 const Background = styled(View)<{ status: BadgeStatuses }>`
   background-color: ${({ status }) => switchBackgroundColor(status)};
