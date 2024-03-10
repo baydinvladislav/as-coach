5d88f6ece75f9a23c4172cf7532b1e29f4fd1677
baydinvladislav <baydinvladislav@yandex.ru>
2024-03-10 17:13:12 +0300

    firebase mock to mocks.py

--- backend/tests/mocks.py
+++ backend/tests/mocks.py
@@ -6,3 +6,9 @@ from unittest.mock import patch
 def mock_send_kafka_message():
     with patch("src.supplier.kafka.KafkaSupplier.send_message") as mock:
         yield mock
+
+
+@pytest.fixture
+def mock_send_notification():
+    with patch("src.service.notifications.notification_service.NotificationService.send_notification") as mock:
+        yield mock
