541761ceb7c83c461df096eb4b2adc92fb71e4f0
baydinvladislav <baydinvladislav@yandex.ru>
2024-03-17 08:40:40 +0300

    fix test for /health endpoint

--- backend/tests/tests.py
+++ backend/tests/tests.py
@@ -7,6 +7,6 @@ from src.main import app
 @pytest.mark.asyncio
 async def test_root(override_get_db):
     async with AsyncClient(app=app, base_url="http://as-coach") as ac:
-        response = await ac.get("/")
+        response = await ac.get("/health")
 
     assert response.status_code == 200
