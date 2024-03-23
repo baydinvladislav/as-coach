be6817baca5dd4970687ccef56b48d1447a1f40d
baydinvladislav <baydinvladislav@yandex.ru>
2024-03-23 15:41:41 +0300

    remove create_exercises fixture from tests

--- backend/tests/api/customer/test_create_training_plan.py
+++ backend/tests/api/customer/test_create_training_plan.py
@@ -11,7 +11,6 @@ from tests.conftest import make_test_http_request
 @pytest.mark.asyncio
 async def test_create_training_plan_successfully(
         create_customer,
-        # create_exercises,
         db,
         mock_send_push_notification,
 ):
@@ -85,7 +84,6 @@ async def test_create_training_plan_successfully(
 @pytest.mark.asyncio
 async def test_create_training_plan_with_supersets_successfully(
         create_customer,
-        # create_exercises,
         db,
         mock_send_push_notification,
 ):
