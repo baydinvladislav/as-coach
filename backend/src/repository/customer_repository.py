cb36edc5ed7b125bbb6a60de115bd3ce9f49c69f
vbaidin <vbaidin@whoosh.bike>
2024-06-07 08:32:07 +0300

    remove model class attribute

--- backend/src/repository/customer_repository.py
+++ backend/src/repository/customer_repository.py
@@ -6,34 +6,30 @@ from src.repository.base_repository import BaseRepository
 
 
 class CustomerRepository(BaseRepository):
-    model = Customer
-
     async def provide_by_pk(self, pk: str) -> Customer | None:
-        query = select(self.model).where(
-            self.model.id == pk
-        ).options(
+        query = (select(Customer).where(Customer.id == pk)
+        .options(
             selectinload(Customer.training_plans).subqueryload(TrainingPlan.trainings),
             selectinload(Customer.training_plans).subqueryload(TrainingPlan.diets)
-        )
+        ))
 
         result = await self.session.execute(query)
         customer = result.scalar_one_or_none()
         return customer
 
     async def provide_by_otp(self, password: str) -> Customer | None:
-        query = select(self.model).where(
-            self.model.password == password
-        ).options(
+        query = (select(Customer).where(Customer.password == password)
+        .options(
             selectinload(Customer.training_plans).subqueryload(TrainingPlan.trainings),
             selectinload(Customer.training_plans).subqueryload(TrainingPlan.diets)
-        )
+        ))
 
         result = await self.session.execute(query)
         customer = result.scalar_one_or_none()
         return customer
 
     async def provide_by_username(self, username: str) -> Customer | None:
-        query = select(self.model).where(self.model.username == username)
+        query = select(Customer).where(Customer.username == username)
         result = await self.session.execute(query)
         customer = result.fetchone()
         return customer
@@ -41,11 +37,11 @@ class CustomerRepository(BaseRepository):
     async def provide_by_coach_id_and_full_name(
         self, coach_id: str, first_name: str, last_name: str
     ) -> Customer | None:
-        query = select(self.model).where(
+        query = select(Customer).where(
             and_(
-                self.model.coach_id == coach_id,
-                self.model.first_name == first_name,
-                self.model.last_name == last_name,
+                Customer.coach_id == coach_id,
+                Customer.first_name == first_name,
+                Customer.last_name == last_name,
             )
         )
         result = await self.session.execute(query)
@@ -55,15 +51,15 @@ class CustomerRepository(BaseRepository):
     async def provide_customers_by_coach_id(self, coach_id: str) -> list[Customer]:
         query = (
             select(
-                self.model.id,
-                self.model.first_name,
-                self.model.last_name,
-                self.model.username,
-                func.max(TrainingPlan.end_date).label('last_plan_end_date')
+                Customer.id,
+                Customer.first_name,
+                Customer.last_name,
+                Customer.username,
+                func.max(TrainingPlan.end_date).label("last_plan_end_date")
             )
-            .join(TrainingPlan, self.model.id == TrainingPlan.customer_id, isouter=True)
-            .where(self.model.coach_id == coach_id)
-            .group_by(self.model.id)
+            .join(TrainingPlan, Customer.id == TrainingPlan.customer_id, isouter=True)
+            .where(Customer.coach_id == coach_id)
+            .group_by(Customer.id)
             .order_by(nullsfirst(func.max(TrainingPlan.end_date).asc()))
         )
 
