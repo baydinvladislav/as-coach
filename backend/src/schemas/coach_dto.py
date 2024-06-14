9f82abcde1b53b756985531bf99bd98119ba349e
vbaidin <vbaidin@whoosh.bike>
2024-06-14 20:43:05 +0300

    create dto schemas files

---
+++ backend/src/schemas/coach_dto.py
@@ -0,0 +1,22 @@
+from datetime import date
+from uuid import UUID
+
+from pydantic import BaseModel
+
+from src import Gender
+
+
+class CoachDtoSchema(BaseModel):
+    id: UUID
+    username: str
+    first_name: str
+    fcm_token: str
+    last_name: str | None
+    password: str
+    gender: Gender | None
+    birthday: date | None
+    email: str | None
+    photo_link: str | None
+
+    class Config:
+        orm_mode = True
--- backend/src/schemas/customer_dto.py
+++ backend/src/schemas/customer_dto.py
@@ -6,23 +6,7 @@ from pydantic import BaseModel
 from src import Gender
 
 
-class UserCoachSchema(BaseModel):
-    id: UUID
-    username: str
-    first_name: str
-    fcm_token: str
-    last_name: str | None
-    password: str
-    gender: Gender | None
-    birthday: date | None
-    email: str | None
-    photo_link: str | None
-
-    class Config:
-        orm_mode = True
-
-
-class UserCustomerSchema(BaseModel):
+class CustomerDtoSchema(BaseModel):
     id: UUID
     username: str | None
     first_name: str
@@ -40,7 +24,7 @@ class UserCustomerSchema(BaseModel):
         orm_mode = True
 
 
-class UserCustomerShort(BaseModel):
+class CustomerShortDtoSchema(BaseModel):
     id: UUID
     first_name: str
     last_name: str | None
