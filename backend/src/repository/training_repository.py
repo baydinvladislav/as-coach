c0ee4083104706aa062df0c21caeb61a19d276d8
baydinvladislav <baydinvladislav@yandex.ru>
2024-06-24 07:09:42 +0300

    training repository refactoring

--- backend/src/repository/training_repository.py
+++ backend/src/repository/training_repository.py
@@ -1,11 +1,17 @@
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from src import Training, ExercisesOnTraining
 from src.repository.base_repository import BaseRepository
+from src.schemas.training_dto import ScheduleExercisesDtoSchema
 
 
-class TrainingRepository(BaseRepository):
-    model = Training
+class TrainingRepository:
+    async def provide_schedule_exercises_by_training_id(self, uow: AsyncSession, training_id: str):
+        query = select(Training).where(Training.id == training_id)
+        result = await uow.execute(query)
+        schedule_exercises = result.scalars().first()
+        return [ScheduleExercisesDtoSchema.from_orm(st) for st in schedule_exercises]
 
 
 class ExercisesOnTrainingRepository(BaseRepository):
