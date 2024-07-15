053d9fc227f5d5fe6e4747dc1d517a6b72856001
baydinvladislav <baydinvladislav@yandex.ru>
2024-07-15 06:45:52 +0300

    remove base repository

--- backend/src/repository/training_repository.py
+++ backend/src/repository/training_repository.py
@@ -4,7 +4,6 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src import Training, ExercisesOnTraining
-from src.repository.base_repository import BaseRepository
 from src.schemas.training_dto import ScheduleExercisesDtoSchema
 
 
@@ -74,17 +73,3 @@ class TrainingRepository:
         uow.add_all(exercises_on_training)
         await uow.flush()
         return len(trainings_orm)
-
-
-class ExercisesOnTrainingRepository(BaseRepository):
-    model = ExercisesOnTraining
-
-    async def filter(self, filters: dict, foreign_keys: list = None, sub_queries: list = None):
-        result = await self.session.execute(
-            select(self.model).order_by(self.model.ordering).where(
-                self.model.training_id.in_(filters["training_ids"])
-            )
-        )
-
-        instances = result.scalars().all()
-        return instances
