234c489a8832a0c2f7228e659b1f1745fa1d7930
baydinvladislav <baydinvladislav@yandex.ru>
2024-07-31 07:55:51 +0300

    reformat

--- backend/src/service/diet_service.py
+++ backend/src/service/diet_service.py
@@ -10,5 +10,9 @@ class DietService:
         self.diet_repository = diet_repository
 
     async def create_diets(self, uow: AsyncSession, training_plan_id: UUID, diets: list) -> int:
-        inserted_row_amount = await self.diet_repository.create_diets(uow, training_plan_id, diets)
+        inserted_row_amount = await self.diet_repository.create_diets(
+            uow=uow,
+            training_plan_id=training_plan_id,
+            diets=diets,
+        )
         return inserted_row_amount
