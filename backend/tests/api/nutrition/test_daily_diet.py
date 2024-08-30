ef4b13343f0af60b6a1f4c713564231955137eac
baydinvladislav <baydinvladislav@yandex.ru>
2024-08-30 12:17:33 +0300

    fix tests

--- backend/tests/api/nutrition/test_daily_diet.py
+++ backend/tests/api/nutrition/test_daily_diet.py
@@ -1,7 +1,9 @@
 from datetime import timedelta
-import pytest
-import uuid
 
+import pytest
+from unittest.mock import patch
+
+from src.schemas.product_dto import ProductDtoSchema
 from tests.conftest import make_test_http_request
 
 
@@ -31,19 +33,46 @@ async def test_get_customer_daily_diet(create_diets):
 
 
 @pytest.mark.asyncio
-async def test_add_product_to_diet(create_diets):
+@patch("src.repository.product_repository.ProductRepository.insert_product")
+async def test_add_product_to_diet(mock_insert_product, create_diets):
     diet_id = str(create_diets[0].id)
     customer_username = create_diets[0].training_plans.customer.username
 
     product_data = {
         "diet_id": diet_id,
         "product_data": [
-            {"id": "406c4cec-a14f-4424-8507-3df2a7954fdd", "amount": 180},
-            {"id": "0422799b-69ab-464f-8535-ef11ee575863", "amount": 220},
+            {"id": "406c4cec-a14f-4424-8507-3df2a7954fdd", "amount": 300},
+            {"id": "0422799b-69ab-464f-8535-ef11ee575863", "amount": 100},
         ],
         "meal_type": "breakfast",
         "specific_day": str(create_diets[0].training_plans.start_date + timedelta(days=2)),
     }
+
+    full_product_info_1 = ProductDtoSchema(
+        id="406c4cec-a14f-4424-8507-3df2a7954fdd",
+        name="Молоко",
+        barcode="123456789",
+        product_type="milliliter",
+        proteins=20,
+        fats=10,
+        carbs=20,
+        calories=250,
+        vendor_name="Простаквашино",
+        user_id=str(create_diets[0].training_plans.customer.id),
+    )
+    full_product_info_2 = ProductDtoSchema(
+        id="0422799b-69ab-464f-8535-ef11ee575863",
+        name="Геркулес",
+        barcode="987654321",
+        product_type="gram",
+        proteins=5,
+        fats=4,
+        carbs=70,
+        calories=330,
+        vendor_name="Простаквашино",
+        user_id=str(create_diets[0].training_plans.customer.id),
+    )
+    mock_insert_product.return_value = [full_product_info_1, full_product_info_2]
 
     response = await make_test_http_request(
         url=f"api/nutrition/diets",
