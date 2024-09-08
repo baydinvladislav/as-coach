d9e4206fee4050d766f8a1beaf4501d59fa5afa6
baydinvladislav <baydinvladislav@yandex.ru>
2024-09-08 10:40:21 +0300

    fix tests

--- backend/src/service/coach_service.py
+++ backend/src/service/coach_service.py
@@ -108,7 +108,7 @@ class CoachService:
 
     async def update_profile(self, uow: AsyncSession, user: Coach, **params) -> CoachDtoSchema | None:
         await self.profile_service.update_user_profile(uow, user, **params)
-        updated_coach = await self.selector_service.select_coach_by_username(uow, params["username"])
+        updated_coach = await self.selector_service.select_coach_by_username(uow, user.username)
         await uow.commit()
         return updated_coach
 
